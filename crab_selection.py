a0fa9322d4990b82147b4864be149324f27d09e6
miceuz <mic@hardcore.lt>
2017-11-21 00:37:52 +0200

    Kintamąjame buf yra ne tik vieno būgno, bet visų būgnų būsena. Išsikeliame krabų skaitliukų ir buf inicializavimą iš analizės ciklo, procentus skaičiuojam paprasčiau, kaip reikės ant mažo MCU be float tipo.

--- crab_selection.py
+++ crab_selection.py
@@ -6,7 +6,7 @@ import random
 def to_percent(a, crabCount):
 	return int(round((a/crabCount)*100, 0))
 
-answers = [0b000000000001, 0b000000000010, 0b000000000011]
+answers = [0b010101010101, 0b101010101010, 0b111111111111, 0b101010010101]
 
 firstCrab = 0
 secondCrab = 0
@@ -14,23 +14,31 @@ thirdCrab = 0
 firstCrabCount = 0
 secondCrabCount = 0
 thirdCrabCount = 0
+MAX_CRAB = 6
+
 for question in range(0, 6):
 	print("Koks tu krabas?")
 
+	firstCrab = secondCrab = thirdCrab = 0;
+	buf = random.choice(answers)
+	print bin(buf) 
 	for wheelPosition in range(0, 6):
 		print wheelPosition
-		buf = random.choice(answers)
 		t = (buf & 0b000000000011)
 		if t == 1: 
+			print "01"
 			firstCrab+=1
 		elif t == 2:
 			secondCrab+=1
+			print "10"
 		elif t == 3:
 			thirdCrab+=1
+			print "11"
 		buf = buf >> 2
+
+	print(firstCrab, secondCrab, thirdCrab)
 	crabCount = firstCrab + secondCrab + thirdCrab
-	firstCrabCount+= firstCrab/crabCount
-	secondCrabCount+= secondCrab/crabCount
-	thirdCrabCount+= thirdCrab/crabCount
-	print(firstCrabCount, secondCrabCount, thirdCrabCount)
-	print("Pirmas", to_percent(firstCrabCount, crabCount), "Antras", to_percent(secondCrabCount, crabCount), "Trecias", to_percent(thirdCrabCount, crabCount))
+	firstCrabPercent = firstCrab * 100 / MAX_CRAB
+	secondCrabPercent = secondCrab * 100 / MAX_CRAB
+	thirdCrabPercent = thirdCrab * 100 / MAX_CRAB
+	print("Pirmas", firstCrabPercent, "Antras", secondCrabPercent, "Trecias", thirdCrabPercent)
