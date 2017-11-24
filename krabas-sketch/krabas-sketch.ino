a740bbe9ff98a2d6ebbc398a7c66b52f9cacde0f
miceuz <mic@hardcore.lt>
2017-11-24 14:31:36 +0200

    Add a 7-seg display test sketch

---
+++ krabas-sketch/krabas-sketch.ino
@@ -0,0 +1,30 @@
+#include<SPI.h>
+#include <Tone.h>
+
+void setup() {
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  pinMode(11, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+
+  digitalWrite(6, LOW);
+
+}
+
+uint8_t t = 1;
+void loop() {
+  digitalWrite(10, LOW);
+  SPI.transfer(t);
+  digitalWrite(10, HIGH);
+  delay(300);
+  t = t << 1;
+  if(0 == t) {
+    t = 1;
+  }
+}
